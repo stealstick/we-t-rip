af414522205d781afd555d4d9d0c3e253429f009
jack <wjdtmddnr24@naver.com>
2016-07-13 09:42:06 +0900

    galauppo
    갈아엎어

---
+++ client/WE-T-RIP2/app/src/main/java/teamapex/kr/we_t_rip/LoginActivity.java
@@ -0,0 +1,201 @@
+package teamapex.kr.we_t_rip;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private AutoCompleteTextView mLoginTextView, mpasswordTextView;
+    private UserLoginTask mAuthTask = null;
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_card_button:
+                attemptLogin();
+                break;
+            case R.id.login_sign_up_button:
+                //TODO 회원가입 창
+                break;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mLoginTextView = (AutoCompleteTextView) findViewById(R.id.login_input_login);
+        mpasswordTextView = (AutoCompleteTextView) findViewById(R.id.login_input_password);
+        findViewById(R.id.login_card_button).setOnClickListener(this);
+        findViewById(R.id.login_sign_up_button).setOnClickListener(this);
+
+    }
+
+
+    private void attemptLogin() {
+        String id = mLoginTextView.getText().toString();
+        String password = mpasswordTextView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+
+   /*비밀번호 유효성 검사*/
+        if (TextUtils.isEmpty(password) || !isPasswordValid(password)) {
+            if (TextUtils.isEmpty(password)) {
+                mpasswordTextView.setError(getString(R.string.error_invalid_password));
+            } else {
+                mpasswordTextView.setError(getString(R.string.error_incorrect_password));
+            }
+            focusView = mpasswordTextView;
+            cancel = true;
+        }
+
+        /*아이디 유효성 검사*/
+        if (TextUtils.isEmpty(id) || !isIdValid(id)) {
+            if (TextUtils.isEmpty(id)) {
+                mLoginTextView.setError(getString(R.string.error_invalid_id));
+
+            } else {
+                mLoginTextView.setError(getString(R.string.error_incorrect_id));
+            }
+            focusView = mLoginTextView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            Toast.makeText(LoginActivity.this, "로그인 작동" + id + password, Toast.LENGTH_SHORT).show();
+            mAuthTask = new UserLoginTask(id, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isIdValid(String id) {
+        Log.d("WET_login_id", String.valueOf(id.contains(" ")));
+        return !id.contains(" ");
+    }
+
+    private boolean isPasswordValid(String password) {
+        Log.d("WET_login_password", String.valueOf(password.length() > 7));
+        return password.length() > 7;
+    }
+
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mId;
+        private final String mPassword;
+        private ProgressDialog dialog;
+
+        UserLoginTask(String id, String password) {
+            mId = id;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+            Log.d("WETTETd", "ASDF");
+            try {
+                HttpURLConnection urlConnection = (HttpURLConnection) new URL("http://teamapex.kr/user/signin.php").openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
+
+                StringBuilder param = new StringBuilder();
+                param.append("&id=" + mId).
+                        append("&passwd=" + mPassword);
+                urlConnection.setRequestMethod("POST");
+
+                urlConnection.connect();
+                DataOutputStream out = new DataOutputStream(urlConnection.getOutputStream());
+                out.writeBytes(param.toString());
+                out.flush();
+
+                BufferedReader bis = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                int responseCode = urlConnection.getResponseCode();
+                Log.d("WETTTTd", String.valueOf(responseCode));
+                Log.d("WETTETd", "ASDF" + bis.readLine());
+                out.close();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            dialog.dismiss();
+            if (success) {
+                finish();
+            } else {
+                Toast.makeText(LoginActivity.this, "로그인 도중 오류가 발생하였습니다. 잠시후에 다시 시도해주세요", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dialog = ProgressDialog.show(LoginActivity.this, "잠시만 기다려주세요", "로그인 중입니다");
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            dialog.dismiss();
+        }
+    }
+
+}
+
--- client/WE-T-RIP2/app/src/main/java/teamapex/kr/we_t_rip/MainActivity.java
+++ client/WE-T-RIP2/app/src/main/java/teamapex/kr/we_t_rip/MainActivity.java
@@ -1,13 +1,9 @@
-package kr.teamapex.we_t_rip.MainPage;
+package teamapex.kr.we_t_rip;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,27 +13,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.squareup.picasso.Picasso;
-
-import kr.teamapex.we_t_rip.AccountStuff.NewLoginActivity;
-import kr.teamapex.we_t_rip.Adapters.MainFragmentAdapter;
-import kr.teamapex.we_t_rip.MainPage.Fragments.PopularFragment;
-import kr.teamapex.we_t_rip.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    private ImageView imageView;
-    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -46,7 +30,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                startActivity(new Intent(MainActivity.this, NewLoginActivity.class));
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
 
@@ -58,24 +42,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        TextView username = (TextView) navigationView.getHeaderView(0).findViewById(R.id.navigation_user_name);
-        username.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, NewLoginActivity.class));
-            }
-        });
-
-
-
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
-        MainFragmentAdapter adapter = new MainFragmentAdapter(getSupportFragmentManager());
-        adapter.addFragment(new PopularFragment(), "추천");
-        adapter.addFragment(new PopularFragment(), "인기");
-        viewPager.setAdapter(adapter);
-
-        tabLayout = (TabLayout) findViewById(R.id.tab);
-        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
@@ -102,6 +68,11 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -109,6 +80,21 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
